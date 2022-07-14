8b6a1da0e9a6b33136723c6d38f96f2aa83365a9
Victoria Martynenko <105857981+v-victoria@users.noreply.github.com>
2022-07-14 10:03:01 +0100

    Added navbar and fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./HomePage";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <Navbar />
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Navbar.css";
+import { ReactComponent as Logo } from "./svg/logo.svg";
+
+export default function Navbar() {
+  return (
+    <div className="Navbar">
+      <nav className="navbar navbar-expand-md">
+        <div className="container-fluid">
+          <a className="navbar-brand mt-2" href="#home">
+            <Logo />
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav">
+              <a className="nav-link" aria-current="page" href="#home">
+                Home
+              </a>
+              <a className="nav-link active" href="#about-me" id="nav-about-me">
+                About me
+              </a>
+              <a className="nav-link" href="#projects" id="nav-projects">
+                Projects
+              </a>
+              <a className="nav-link" href="#contacts" id="nav-contacts">
+                Contacts
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="navbar-size-replace d-none"></div>
+    </div>
+  );
+}
