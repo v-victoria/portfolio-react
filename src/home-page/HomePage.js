959a1c1ed34909fb613d851b3108b4b102973638
Victoria Martynenko <105857981+v-victoria@users.noreply.github.com>
2022-07-25 14:39:34 +0100

    Added react-reveal and homepage animation

--- src/home-page/HomePage.js
+++ src/home-page/HomePage.js
@@ -1,27 +1,35 @@
 import React from "react";
 import "./HomePage.css";
 import homeImg from "../svg/homepage-img.svg";
+import Zoom from "react-reveal/Zoom";
+import Slide from "react-reveal/Fade";
 
 export default function HomePage() {
   return (
     <div className="HomePage" id="home">
       <div className="center-block">
         <div className="text-block">
-          <div className="name-block">
-            Hello, my name is <h2 className="name">Victoria</h2>.
-          </div>
-          <div className="profession-block">
-            I'm a{" "}
-            <h1 className="profession text-underline-fist-color">
-              Front-end Developer
-            </h1>
-            <br />
-            based in England.
-          </div>
+          <Slide left delay={0}>
+            <div className="name-block">
+              Hello, my name is <h2 className="name">Victoria</h2>.
+            </div>
+          </Slide>
+          <Slide left delay={1000}>
+            <div className="profession-block">
+              I'm a{" "}
+              <h1 className="profession text-underline-fist-color">
+                Front-end Developer
+              </h1>
+              <br />
+              based in England.
+            </div>
+          </Slide>
         </div>
-        <img src={homeImg} alt="homepage girl" />
+        <Zoom delay={2000}>
+          <img src={homeImg} alt="homepage girl" />
+        </Zoom>
       </div>
-      <a href="/#about-me">
+      <a href="/#about-me" className="d-none d-md-inline">
         <i className="fa-solid fa-arrow-down-long"></i>
       </a>
     </div>
